35203ee251ab4bb5c80cea2a6f12b7afe425ba68
KildrenVX <migux07@gmail.com>
2017-05-01 18:07:12 -0300

    Initial commit

---
+++ app/src/main/java/com/example/m/tutoriallistview/Connector.java
@@ -0,0 +1,33 @@
+package com.example.m.tutoriallistview;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by m on 01-05-2017.
+ */
+
+public class Connector {
+    public static Object connect(String urlAddress)
+    {
+        try
+        {
+            URL url=new URL(urlAddress);
+            HttpURLConnection con= (HttpURLConnection) url.openConnection();
+            //SET CON PROPERTIES
+            con.setRequestMethod("GET");
+            con.setConnectTimeout(15000);
+            con.setReadTimeout(15000);
+            con.setDoInput(true);
+            return con;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return "Error "+e.getMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error "+e.getMessage();
+        }
+    }
+}
